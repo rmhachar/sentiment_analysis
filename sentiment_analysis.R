9bf9097d3423c0506f777517408f232c95bbeb19
rmhachar <richard.hachar@gmail.com>
2018-03-18 10:48:36 -0500

    Update sentiment_analysis.R

--- sentiment_analysis.R
+++ sentiment_analysis.R
@@ -1,1 +1,13 @@
+# STEP 1: Import Packages
+library(twitteR)
+library(ROAuth)
+
+# STEP 2: Load Consumer Keys
+consumer_key <- "your_info_here" # replace with your info
+consumer_secret <- "your_info_here"
+access_token <- "your_info_here"
+access_secret <- "your_info_here"
+
+# Set up the connection
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
